2e6b7633b359afa6a36085da8f77460bfa61db96
Nostramarkus <markvanlit@gmail.com>
2019-03-07 12:21:12 +0100

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,10 +1,19 @@
 function filter(bodyType) {
 
+    /*
+    Je maakt hier een ajax post op de 'plain' javascript manier. jQuery heeft ook een paar manieren voor ajax posts te maken
+    Een makkelijke vind ik zelf $.post() --> https://www.w3schools.com/jquery/ajax_post.asp
+    */
+    
     var xmlhttp = new XMLHttpRequest();
-
+    
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
 
+            /*
+            Als je toch jQuery gebruikt waarom zou je dan niet de response met jquery toevoegen? Zo dus:
+            $("#filter").html(this.responseText);
+            */
             document.getElementById("filter").innerHTML = this.responseText;
     
         }else if(this.status == 404){
