dfb7374244e3a22e1b32da89afdba7e874fcbecb
Nostramarkus <markvanlit@gmail.com>
2019-03-07 11:54:29 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+/* 
+Hi Tim!
+
+Gebruik $(document).ready(... altijd maar 1 keer.
+jQuery werkt met zogenaamde css-selectors, dwz dat je elementen op de pagina kunt beetp[akken net zoals je dit met css 
+zou doen. 
+
+Een voorbeeld met css:
+#mijnIdSelector{
+ ...
+}
+
+Een voorbeeld met jQuery:
+$("#mijnIdSelector")...
+
+Om een selector te kunnen vinden moet je html document volledig ingeladen zijn. Precies hiervoor dient de $(document).ready
+Alles wat binnen $(document).ready staat wordt pas uitgevoerd indien je html document volledig klaar is met laden. Daarom
+heeft een js bestand wat van jQuery gebruik maakt altijd maar één keer $(document).ready erin staan.
+
+De rest van deze code ziet er wel oke uit
+
+Gr Mark
+*/
+
+
 // Open and close filter div on model-overview page
 
 $(document).ready(function(){
